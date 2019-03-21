81adec0393000644539af45bbfbd078cee113dfc
Gergely Imreh <gergely@balena.io>
2019-03-21 18:37:29 +0000

    Update the notes section of the run.sh
    Change-type: patch
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 # This oneiner does the following:
 # * takes UUIDs from the 'batch' file
 # * in parallel runs the update logic on them, with parallelism defined by the '-P' setting
-# * if there's a success logged for a UUID, nothing is run
-# * if no success logged, then check if UUID exists first,
-#   then connect to the device with balena ssh, pipe in the task script, and
+# * connect to the device with balena ssh, pipe in the task script, and
 #   save the log with the UUID prepended
 cat batch | stdbuf -oL xargs -I{} -P 10 /bin/sh -c "grep -a -q '{} : DONE' sshkey.log || (cat add-ssh-key.sh | balena ssh {} -s --noninteractive | sed 's/^/{} : /' | tee --append sshkey.log)"
