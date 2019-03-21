5c2128b5fb64f39402af7d675137649d6e8d6db3
Gergely Imreh <gergely@balena.io>
2019-03-21 16:38:36 +0000

    Do not allow parallel running of the script on the same device
    Change-type: patch
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- add-ssh-key.sh
+++ add-ssh-key.sh
@@ -6,7 +6,8 @@ set -o errexit -o pipefail
 # shellcheck disable=SC1091
 source /etc/profile
 
-# Edit this to add your ssh-key:
+# Edit the SSHKEY variable to add your ssh-key, similar to this example:
+# SSHKEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3rIsl4KO2zasaRSC4U6eauGqy5E6zuq4wgApKfzXjjIdtNHfYMC28CCCJvDbbaM2qx02z1x2XsxhvsIVI5+8VNNMXiy9/KRZGqpi1DK4R41k5NgyXW1RtU4CfOU4nFriVif1xq7d96qJTfvDUS47Vbr2aRT001Gq5Qh5Oo+p+YQVhWqn1I4A4VEYCXp69Vn/agZTww6yGnQRCU4Du5WKOTfrEw/BPbNLhndPNejgES+lPiGjTDW3m9rFaWM99TwuI7vQ6Gi+GXwfPCWlhR1frh9fifT8PFw9hhaoTv8q+f/hBuIOcfmWYZ38JfCWrgvYGfNoMiGNY33dd19CmJXgf nobody@nowhere"
 SSHKEY=""
 
 if [ -z "$SSHKEY" ]; then
@@ -31,5 +32,10 @@ main() {
   fi
 }
 
-main
-exit 0
+(
+  # Check if already running and bail if yes
+  flock -n 99 || (echo "Already running script..."; exit 1)
+  main
+) 99>/tmp/sshkey.lock
+# Proper exit, required due to the locking subshell
+exit $?
